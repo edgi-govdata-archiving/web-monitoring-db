68f484f184d73d9d488a8cf1938333e1ea8750f1
Rob Brackett <rob@robbrackett.com>
2017-02-23 00:12:57 -0800

    Add ruby version to gemfile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,11 @@ class PagesController < ApplicationController
   def index
     # TODO: gem for paging?
     @total_pages = VersionistaPage.all.count
-    @total_page_groups = (@total_pages / PAGE_GROUP_SIZE).ceil
+    @total_page_groups = if @total_pages == 0
+      1
+    else
+      (@total_pages / PAGE_GROUP_SIZE).ceil
+    end
     @page_group_number = (params[:page] || 1).to_i.clamp(1, @total_page_groups)
     
     offset = (@page_group_number - 1) * PAGE_GROUP_SIZE
