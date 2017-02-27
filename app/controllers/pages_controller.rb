1b2c4a9576a9d2079d5e4731316c81fcf361c703
Rob Brackett <rob@robbrackett.com>
2017-02-27 14:58:42 -0800

    Add basic JSON API for reading
    This basically follows the JSON:api spec, though it uses the wrong content type ("application/json").

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,67 @@
-PAGE_GROUP_SIZE = 500.0
+PAGE_SIZE = 500.0
 
 class PagesController < ApplicationController
   def index
-    # TODO: gem for paging?
-    @total_pages = VersionistaPage.all.count
-    @total_page_groups = if @total_pages == 0
-      1
-    else
-      (@total_pages / PAGE_GROUP_SIZE).ceil
-    end
-    @page_group_number = (params[:page] || 1).to_i.clamp(1, @total_page_groups)
+    @paging = pagination(VersionistaPage.all)
+    @pages = VersionistaPage.order(updated_at: :desc).limit(PAGE_SIZE).offset(@paging[:offset])
     
-    offset = (@page_group_number - 1) * PAGE_GROUP_SIZE
-    @pages = VersionistaPage.order(updated_at: :desc).limit(PAGE_GROUP_SIZE).offset(offset)
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          links: @paging[:links],
+          data: @pages
+        }
+      end
+    end
   end
   
   def show
     @page = VersionistaPage.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          data: @page.as_json(include: :versions)
+        }
+      end
+    end
+  end
+  
+  protected
+  
+  # Undoubtedly there is a gem that makes this nicer
+  def pagination(collection=nil)
+    unless collection
+      collection = @collection || VersionistaPage.all
+    end
+    
+    format_type = request.format.to_sym
+    total_items = collection.count
+    total_pages = total_items == 0 ? 1 : (total_items / PAGE_SIZE).ceil
+    page_number = (params[:page] || 1).to_i.clamp(1, total_pages)
+    page_offset = (page_number - 1) * PAGE_SIZE
+    
+    links = {
+      first: pages_path(nil, page: 1, format: format_type),
+      last: pages_path(nil, page: total_pages, format: format_type),
+      prev: nil,
+      next: nil
+    }
+    if page_number > 1
+      links[:prev] = pages_path(nil, page: page_number - 1, format: format_type)
+    end
+    if page_number < total_pages
+      links[:next] = pages_path(nil, page: page_number + 1, format: format_type)
+    end
+    
+    {
+      pages: total_pages,
+      page_number: page_number,
+      offset: page_offset,
+      total_items: total_items,
+      page_items: PAGE_SIZE,
+      links: links
+    }
   end
 end
