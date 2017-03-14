49d134b5f170be607e1895a10d9d299490d8c78f
Rob Brackett <rob@robbrackett.com>
2017-03-14 16:11:52 -0700

    Migrate old data into new generic models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313192244) do
+ActiveRecord::Schema.define(version: 20170314195641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20170313192244) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "versionista_account"
+    t.string   "uuid"
     t.index ["url"], name: "index_versionista_pages_on_url", using: :btree
   end
 
