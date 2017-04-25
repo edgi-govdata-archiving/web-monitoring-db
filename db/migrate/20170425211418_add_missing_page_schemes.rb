29f3d2d386d0a04979a0d9dee2ea0682bd7e616a
Rob Brackett <rob@robbrackett.com>
2017-04-25 14:20:45 -0700

    Add migration to normalize URLs (fixes #25)

---
+++ db/migrate/20170425211418_add_missing_page_schemes.rb
@@ -0,0 +1,11 @@
+class AddMissingPageSchemes < ActiveRecord::Migration[5.0]
+  def up
+    Page.where("url NOT LIKE 'http://%' AND url NOT LIKE 'https://%' AND url NOT LIKE 'ftp://%'").each do |page|
+      page.save
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "This migration modifies existing bad data and cannot be reversed."
+  end
+end
