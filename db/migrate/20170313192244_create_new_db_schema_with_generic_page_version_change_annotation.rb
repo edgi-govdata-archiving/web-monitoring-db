77b638062d1ba1f825b2d2c3b898ca95466bbeaa
Rob Brackett <rob@robbrackett.com>
2017-04-25 10:11:11 -0700

    Remove PG/MySQL UUID/JSON compatibility code
    This migration was pretty ridiculous and promised pain down the road. Now that PG is available on Google Cloud and we aren't so sure about being tied to Google Cloud anyway, we should just standardize on PG and use its features without these kinds of contortions.

--- db/migrate/20170313192244_create_new_db_schema_with_generic_page_version_change_annotation.rb
+++ db/migrate/20170313192244_create_new_db_schema_with_generic_page_version_change_annotation.rb
@@ -1,29 +1,10 @@
 class CreateNewDbSchemaWithGenericPageVersionChangeAnnotation < ActiveRecord::Migration[5.0]
-  def initialize(*args)
-    super(*args)
-    db_connection = ActiveRecord::Base.connection
-    @uuid_type = db_connection.valid_type?(:uuid) ? :uuid : :string
-    @json_type = db_connection.valid_type?(:jsonb) ? :jsonb : :json
-  end
-
-  def add_uuid(table, column, options = {})
-    if @uuid_type == :string
-      options = options.merge({limit: 36})
-    end
-
-    if column == :primary_key
-      table.primary_key :uuid, @uuid_type, options
-    else
-      table.send @uuid_type, column, options
-    end
-  end
-
   def change
     # Postgres needs this for proper UUID support
     enable_extension 'uuid-ossp'
 
     create_table :pages, id: false do |t|
-      add_uuid t, :primary_key
+      table.primary_key :uuid, :uuid
       t.string :url, null: false
       t.string :title
       t.string :agency
@@ -34,14 +15,14 @@ class CreateNewDbSchemaWithGenericPageVersionChangeAnnotation < ActiveRecord::Mi
     end
 
     create_table :versions, id: false do |t|
-      add_uuid t, :primary_key
+      table.primary_key :uuid, :uuid
       # No way to use `belongs_to/references` to make a column named `*_uuid`
-      add_uuid t, :page_uuid, null: false
+      table.uuid :page_uuid, null: false
       t.datetime :capture_time, null: false
       t.string :uri
       t.string :version_hash
       t.string :source_type
-      t.send @json_type, :source_metadata
+      t.jsonb :source_metadata
       t.timestamps
 
       t.index :page_uuid
@@ -50,11 +31,11 @@ class CreateNewDbSchemaWithGenericPageVersionChangeAnnotation < ActiveRecord::Mi
     end
 
     create_table :changes, id: false do |t|
-      add_uuid t, :primary_key
-      add_uuid t, :uuid_from, null: false
-      add_uuid t, :uuid_to, null: false
+      table.primary_key :uuid, :uuid
+      t.uuid :uuid_from, null: false
+      t.uuid :uuid_to, null: false
       t.float :priority, default: 0.5
-      t.send @json_type, :current_annotation
+      t.jsonb :current_annotation
       t.timestamps
 
       t.index :uuid_to
@@ -64,11 +45,11 @@ class CreateNewDbSchemaWithGenericPageVersionChangeAnnotation < ActiveRecord::Mi
     end
 
     create_table :annotations, id: false do |t|
-      add_uuid t, :primary_key
+      table.primary_key :uuid, :uuid
       # No way to use `belongs_to/references` to make a column named `*_uuid`
-      add_uuid t, :change_uuid, null: false
+      t.uuid :change_uuid, null: false
       t.belongs_to :author, foreign_key: {to_table: :users}
-      t.send @json_type, :annotation, null: false
+      t.jsonb :annotation, null: false
       t.timestamps
 
       t.index :change_uuid
