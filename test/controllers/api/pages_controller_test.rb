a3d422dac2aad3f7dbaedf25d123033ca88e3a9a
Rob Brackett <rob@robbrackett.com>
2017-04-25 17:56:56 -0700

    Add a simplistic controller test
    We need to do more of these!

--- test/controllers/api/pages_controller_test.rb
+++ test/controllers/api/pages_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Api::PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can list pages' do
+    get '/api/v0/pages/'
+    assert_response :success
+    assert_equal 'application/json', @response.content_type
+    body_json = JSON.parse @response.body
+    assert body_json.has_key?('links'), 'Response should havea  "links" property'
+    assert body_json.has_key?('data'), 'Response should havea  "data" property'
+  end
 end
