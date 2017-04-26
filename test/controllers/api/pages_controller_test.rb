7f1556c6dcb7f95dd813ce4d81d550e40e967cf4
Rob Brackett <rob@robbrackett.com>
2017-04-26 11:48:26 -0700

    Fix rubocop errors and two typos

--- test/controllers/api/pages_controller_test.rb
+++ test/controllers/api/pages_controller_test.rb
@@ -6,7 +6,7 @@ class Api::PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal 'application/json', @response.content_type
     body_json = JSON.parse @response.body
-    assert body_json.has_key?('links'), 'Response should havea  "links" property'
-    assert body_json.has_key?('data'), 'Response should havea  "data" property'
+    assert body_json.key?('links'), 'Response should have a "links" property'
+    assert body_json.key?('data'), 'Response should have a "data" property'
   end
 end
