2b8c4958e9b91845850b0edd5acc20fc9c40105d
Rob Brackett <rob@robbrackett.com>
2017-04-26 20:54:36 -0700

    Add POST /api/v0/pages/{id}/versions route
    This is stupendously ugly, has many caveats and needs cleanup, but it's the basics

--- app/controllers/api/v0/versions_controller.rb
+++ app/controllers/api/v0/versions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V0::VersionsController < Api::V0::ApiController
   def index
-    page = Page.find(params[:page_id])
     paging = pagination(page.versions)
     versions = page.versions.limit(paging[:page_items]).offset(paging[:offset])
 
@@ -11,7 +10,6 @@ class Api::V0::VersionsController < Api::V0::ApiController
   end
 
   def show
-    page = Page.find(params[:page_id])
     version = page.versions.find(params[:id])
     render json: {
       links: {
@@ -22,9 +20,72 @@ class Api::V0::VersionsController < Api::V0::ApiController
     }
   end
 
+  def create
+    @version = page.versions.new(version_params)
+    Page.find('abcd')
+    if @version.uri.nil?
+      if params[:content]
+        # TODO: upload content
+        raise Api::NotImplementedError, 'Raw content uploading not implemented yet.'
+      else
+        raise Api::InputError, 'You must include raw version content in the `content` field if you do not provide a URI.'
+      end
+    elsif !allowed_version_uri?(@version.uri)
+      # TODO: download and upload to S3
+      raise Api::NotImplementedError, 'Raw content retrieval not implemented yet.'
+    end
+
+    @version.validate!
+
+    existing = page.versions.where(
+      capture_time: @version.capture_time,
+      source_type: @version.source_type
+    ).first
+
+    if existing
+      redirect_to api_v0_page_version_url(existing.page, existing)
+    else
+      @version.save
+      render json: {
+        links: {
+          page: api_v0_page_url(@version.page),
+          previous: api_v0_page_version_url(@version.page, @version.previous)
+        },
+        data: @version.as_json(methods: :current_annotation)
+      }
+    end
+  end
+
   protected
 
   def paging_path_for_version(*args)
     api_v0_page_versions_path(*args)
+  end
+
+  def page
+    @page ||= Page.find(params[:page_id])
+  end
+
+  def version_params
+    # Use select instead of `permit` to get the metadata blob. (It's freeform,
+    # so we don't know what keys will be in it.)
+    params.require(:version).select do |key|
+      [
+        'uuid',
+        'capture_time',
+        'uri',
+        'version_hash',
+        'source_type',
+        'source_metadata'
+      ].include?(key)
+    end.permit!
+  end
+
+  def allowed_version_uri?(uri)
+    return false if uri.nil?
+
+    # FIXME: this parsing should be in an initializer
+    allowed_hosts = ENV.fetch('ALLOWED_VERSION_HOSTS', '').split(' ')
+    allowed_hosts.any? {|base| uri.starts_with?(base)}
   end
 end
