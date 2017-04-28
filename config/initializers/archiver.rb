eef4790f0866a11205f38a7bdb8ef5a14fbe41ca
Rob Brackett <rob@robbrackett.com>
2017-04-28 13:38:10 -0700

    Only initialize archiver if config is present

--- config/initializers/archiver.rb
+++ config/initializers/archiver.rb
@@ -1,10 +1,15 @@
 require_dependency Rails.root.join('lib/archiver/archiver')
 require_dependency Rails.root.join('lib/archiver/stores/s3')
 
-Archiver.store = Archiver::Stores::S3.new(
-  key: ENV['AWS_S3_KEY'],
-  secret: ENV['AWS_S3_SECRET'],
-  region: ENV['AWS_S3_REGION'],
-  bucket: ENV['AWS_S3_BUCKET']
-)
-Archiver.allowed_hosts = ENV['ALLOWED_ARCHIVE_HOSTS']
+if ENV['AWS_S3_KEY'] && ENV['AWS_S3_SECRET'] && ENV['AWS_S3_BUCKET']
+  Archiver.store = Archiver::Stores::S3.new(
+    key: ENV['AWS_S3_KEY'],
+    secret: ENV['AWS_S3_SECRET'],
+    region: ENV['AWS_S3_REGION'],
+    bucket: ENV['AWS_S3_BUCKET']
+  )
+end
+
+if ENV['ALLOWED_ARCHIVE_HOSTS']
+  Archiver.allowed_hosts = ENV['ALLOWED_ARCHIVE_HOSTS']
+end
