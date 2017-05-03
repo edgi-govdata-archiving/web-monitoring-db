ebcd6656f0cf6deb3fc321d8bfb9f8d788a26b13
Rob Brackett <rob@robbrackett.com>
2017-05-03 12:09:31 -0700

    Hide non-standard API on Archiver::Stores::S3

--- test/lib/archiver/stores/s3_test.rb
+++ test/lib/archiver/stores/s3_test.rb
@@ -12,31 +12,31 @@ class Archiver::Stores::S3Test < ActiveSupport::TestCase
   end
 
   test 's3 storage can parse S3 URIs' do
-    parsed = test_storage.parse_s3_url('s3://test-bucket/some/thing.txt')
+    parsed = test_storage.send :parse_s3_url, 's3://test-bucket/some/thing.txt'
     assert_equal 'test-bucket', parsed[:bucket]
     assert_equal 'some/thing.txt', parsed[:path]
     assert_nil parsed[:region]
   end
 
   test 's3 storage can parse S3 URLs' do
-    bucket_hostname_url = test_storage.parse_s3_url('https://test-bucket.s3.amazonaws.com/some/thing.txt')
+    bucket_hostname_url = test_storage.send :parse_s3_url, 'https://test-bucket.s3.amazonaws.com/some/thing.txt'
     assert_equal 'test-bucket', bucket_hostname_url[:bucket]
     assert_equal 'some/thing.txt', bucket_hostname_url[:path]
     assert_nil bucket_hostname_url[:region]
 
-    bucket_path_url = test_storage.parse_s3_url('https://s3.amazonaws.com/test-bucket/some/thing.txt')
+    bucket_path_url = test_storage.send :parse_s3_url, 'https://s3.amazonaws.com/test-bucket/some/thing.txt'
     assert_equal 'test-bucket', bucket_path_url[:bucket]
     assert_equal 'some/thing.txt', bucket_path_url[:path]
     assert_nil bucket_path_url[:region]
 
-    region_url = test_storage.parse_s3_url('https://s3-us-west-2.amazonaws.com/test-bucket/some/thing.txt')
+    region_url = test_storage.send :parse_s3_url, 'https://s3-us-west-2.amazonaws.com/test-bucket/some/thing.txt'
     assert_equal 'test-bucket', region_url[:bucket]
     assert_equal 'some/thing.txt', region_url[:path]
     assert_equal 'us-west-2', region_url[:region]
   end
 
   test 's3 storage will not parse non-S3 URLs' do
-    parsed = test_storage.parse_s3_url('https://google.com/some/thing.txt')
+    parsed = test_storage.send :parse_s3_url, 'https://google.com/some/thing.txt'
     assert_nil parsed
   end
 
