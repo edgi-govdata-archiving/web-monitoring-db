82923a15e77bb4b4ed2ff821438ab58f4acbe461
Rob Brackett <rob@robbrackett.com>
2017-05-05 13:24:19 -0700

    Add local file storage

---
+++ test/lib/archiver/stores/local_file_test.rb
@@ -0,0 +1,42 @@
+require_dependency Rails.root.join('lib/archiver/stores/local_file')
+
+class Archiver::Stores::LocalFileTest < ActiveSupport::TestCase
+  storage_path = Rails.root.join('tmp/test/storage')
+
+  def setup
+    @storage = nil
+  end
+
+  def storage(options = {})
+    @storage ||= Archiver::Stores::LocalFile.new(options)
+  end
+
+  test 'can save a file' do
+    Archiver::Stores::LocalFile.new.save_file 'abc', 'xyz'
+    Archiver::Stores::LocalFile.new(path: storage_path).save_file 'abc', 'xyz'
+  end
+
+  test 'can get a file' do
+    storage.save_file 'abc', 'xyz'
+    assert_equal 'xyz', storage.get_file('abc'), 'The retrieved file did not match what was saved'
+  end
+
+  test 'correctly matches file URLs' do
+    storage.save_file 'abc', 'xyz'
+    url = storage.url_for_file 'abc'
+    assert storage.contains_url?(url)
+  end
+
+  test 'does not match non-file URLs' do
+    assert_not storage.contains_url?('http://somewhere.com')
+  end
+
+  test 'does not match file URLs that are not in its directory' do
+    assert_not storage.contains_url?('file:///nowhere')
+  end
+
+  test 'can generate a file URL' do
+    whatever_url = "file://#{storage_path.join 'whatever'}"
+    assert_equal whatever_url, storage(path: storage_path).url_for_file('whatever')
+  end
+end
