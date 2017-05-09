2ac4eb9f56cec9685ab12678ce54364186a5919a
Rob Brackett <rob@robbrackett.com>
2017-05-08 17:05:28 -0700

    Touch Page#updated_at when updating versions

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,7 @@
 class Version < ApplicationRecord
   include UuidPrimaryKey
 
-  belongs_to :page, foreign_key: :page_uuid, required: true, inverse_of: :versions
+  belongs_to :page, foreign_key: :page_uuid, required: true, inverse_of: :versions, touch: true
   has_many :tracked_changes, class_name: 'Change', foreign_key: 'uuid_to'
   has_many :invalid_changes,
     ->(version) { where.not(uuid_from: version.previous.uuid) },
