6e81e42a5bc32e691698a00ef1e2f0d58f010401
Rob Brackett <rob@robbrackett.com>
2017-05-17 15:52:20 -0700

    Make annotations.author_id NOT NULL in the DB
    The schema change is rather large here; I think that's because we switched Rails 5.0 -> 5.1 (which is actually a big jump).

---
+++ db/migrate/20170517222310_change_annotation_author_to_not_null.rb
@@ -0,0 +1,23 @@
+class ChangeAnnotationAuthorToNotNull < ActiveRecord::Migration[5.1]
+  def do_sql(*args)
+    expression = ActiveRecord::Base.send :sanitize_sql, args
+    ActiveRecord::Base.connection.exec_query(expression)
+  end
+
+  def up
+    default_author_id = do_sql(
+      'SELECT id FROM users ORDER BY created_at ASC LIMIT 1'
+    ).first['id']
+
+    do_sql(
+      'UPDATE annotations SET author_id = ? WHERE author_id IS NULL',
+      default_author_id
+    )
+
+    change_column_null :annotations, :author_id, false
+  end
+
+  def down
+    change_column_null :annotations, :author_id, true
+  end
+end
