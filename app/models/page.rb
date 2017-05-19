1522eefc63af8c69dc9a4dfe4ab2eab7ebcd35a2
Rob Brackett <rob@robbrackett.com>
2017-05-19 16:06:49 -0700

    Reformat Page#latest, add explanatory comments

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,11 +2,18 @@ class Page < ApplicationRecord
   include UuidPrimaryKey
 
   has_many :versions, -> { order(capture_time: :desc) }, foreign_key: 'page_uuid', inverse_of: :page
-  has_one :latest, (lambda do
-    relation = order('versions.page_uuid')
-    relation.select_values = ['DISTINCT ON (versions.page_uuid) versions.*']
-    relation.order('versions.capture_time DESC')
-  end), foreign_key: 'page_uuid', class_name: 'Version'
+  has_one :latest,
+    (lambda do
+      # DISTINCT ON requires the first ORDER to be the distinct column(s)
+      relation = order('versions.page_uuid')
+      # HACK: This is not public API, but I couldn't find a better way. The
+      # `DISTINCT ON` statement has to be at the start of the WHERE clause, but
+      # all public methods append to the end.
+      relation.select_values = ['DISTINCT ON (versions.page_uuid) versions.*']
+      relation.order('versions.capture_time DESC')
+    end),
+    foreign_key: 'page_uuid',
+    class_name: 'Version'
 
   before_save :normalize_url
   validate :url_must_have_domain
