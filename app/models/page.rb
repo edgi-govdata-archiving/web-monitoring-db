361917bdd75d561d3785bcf81ee3b52060295ce3
Rob Brackett <rob@robbrackett.com>
2017-05-19 19:02:58 -0700

    Optimize the basic page listing call in the API (#52)
    Make Page#latest an association & preload it

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,18 @@ class Page < ApplicationRecord
   include UuidPrimaryKey
 
   has_many :versions, -> { order(capture_time: :desc) }, foreign_key: 'page_uuid', inverse_of: :page
+  has_one :latest,
+    (lambda do
+      # DISTINCT ON requires the first ORDER to be the distinct column(s)
+      relation = order('versions.page_uuid')
+      # HACK: This is not public API, but I couldn't find a better way. The
+      # `DISTINCT ON` statement has to be at the start of the WHERE clause, but
+      # all public methods append to the end.
+      relation.select_values = ['DISTINCT ON (versions.page_uuid) versions.*']
+      relation.order('versions.capture_time DESC')
+    end),
+    foreign_key: 'page_uuid',
+    class_name: 'Version'
 
   before_save :normalize_url
   validate :url_must_have_domain
@@ -13,16 +25,6 @@ class Page < ApplicationRecord
     else
       "http://#{url}"
     end
-  end
-
-  # A serialized page should always include some version info. If expanded
-  # version objects weren't requested, it includes the latest version.
-  def as_json(*args)
-    result = super(*args)
-    if result['versions'].nil?
-      result['latest'] = self.versions.first.as_json
-    end
-    result
   end
 
   protected
