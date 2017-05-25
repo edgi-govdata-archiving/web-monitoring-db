359f43783536d35c2d2f8f589b864a9903b1360d
Rob Brackett <rob@robbrackett.com>
2017-05-25 16:57:30 -0700

    Send e-mails with Postmark! Yay! Fixes #50

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   end
 
   # Mailers
-  config.action_mailer.default_url_options = { :host => ENV.fetch('HOST_URL', 'localhost:3000') }
+  config.action_mailer.default_url_options = {
+    :host => ENV.fetch('HOST_URL', 'localhost:3000')
+  }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
