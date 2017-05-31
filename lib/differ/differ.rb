7517eafa8c143c1c8337ce7fd1797108b880a43e
Rob Brackett <rob@robbrackett.com>
2017-05-31 00:59:46 -0700

    Switch module attrs from class to instance style
    Make Rubocop happy. This syntax is only meaningfully different when modules are used with classes, which we aren't doing. For our use, they're equivalent.

--- lib/differ/differ.rb
+++ lib/differ/differ.rb
@@ -1,15 +1,13 @@
 module Differ
-  @@type_map = nil
-
   # Register a differ instance to be used for a given diff type
   def self.register(type, differ)
-    @@type_map ||= {}.with_indifferent_access
-    @@type_map[type] = differ
+    @type_map ||= {}.with_indifferent_access
+    @type_map[type] = differ
   end
 
   # Retrieve the differ associated with a given diff type
   def self.for_type(type)
-    @@type_map ? @@type_map[type] : nil
+    @type_map ? @type_map[type] : nil
   end
 
   def self.for_type!(type)
