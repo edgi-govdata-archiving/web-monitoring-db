014fa1cb429f15e2eebc9736aabfc686f2cf4c42
Rob Brackett <rob@robbrackett.com>
2017-07-13 16:56:43 -0700

    Make Change.priority nullable + null-by-default

---
+++ db/migrate/20170713234650_set_change_priority_default_to_null.rb
@@ -0,0 +1,20 @@
+class SetChangePriorityDefaultToNull < ActiveRecord::Migration[5.1]
+  def do_sql(*args)
+    expression = ActiveRecord::Base.send :sanitize_sql, args
+    ActiveRecord::Base.connection.exec_query(expression)
+  end
+
+  def up
+    change_column_null :changes, :priority, true
+    change_column_default :changes, :priority, from: 0.5, to: nil
+
+    do_sql('UPDATE changes SET priority = ? WHERE priority = 0.5', nil)
+  end
+
+  def down
+    do_sql('UPDATE changes SET priority = ? WHERE priority IS NULL', 0.5)
+
+    change_column_default :changes, :priority, from: nil, to: 0.5
+    change_column_null :changes, :priority, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517222310) do
+ActiveRecord::Schema.define(version: 20170713234650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170517222310) do
   create_table "changes", primary_key: "uuid", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.uuid "uuid_from", null: false
     t.uuid "uuid_to", null: false
-    t.float "priority", default: 0.5
+    t.float "priority"
     t.jsonb "current_annotation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
