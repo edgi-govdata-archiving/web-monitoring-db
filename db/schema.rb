8d52bfea645da35f4abc8ddd71f613e180d64827
Rob Brackett <rob@robbrackett.com>
2017-07-17 00:34:09 -0700

    Add significance to Change table in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713234650) do
+ActiveRecord::Schema.define(version: 20170717072416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170713234650) do
     t.jsonb "current_annotation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "significance"
     t.index ["uuid_to", "uuid_from"], name: "index_changes_on_uuid_to_and_uuid_from", unique: true
     t.index ["uuid_to"], name: "index_changes_on_uuid_to"
   end
