782d55b4f499ecd9d55c0686bc7a32d8853bd130
Rob Brackett <rob@robbrackett.com>
2017-09-05 18:22:58 -0700

    Fix previous ordering/paging bug better
    Instead of sorting in Ruby code, emulate the two-step query ActiveRecord automatically does (but work around the includes/order problem) so that we get results back in the right order. This still results in only two queries, but we get the results back pre-sorted from the DB, so it should hopefully be a little faster (especially if we increase the max paging size).

--- app/controllers/api/v0/pages_controller.rb
+++ app/controllers/api/v0/pages_controller.rb
@@ -4,10 +4,8 @@ class Api::V0::PagesController < Api::V0::ApiController
     paging = pagination(query)
     pages = query.limit(paging[:page_items]).offset(paging[:offset])
 
-    # If we are including versions, sort the version list in-app here.
-    # Why is a bit complicated... when using limit/offset on queries that
-    # `include` associations (as we include `versions` here), it actually does
-    # TWO queries:
+    # When using limit/offset on queries that `include` associations (e.g.
+    # `versions` here), ActiveRecord actually does TWO queries:
     #   1. A query for only the IDs of the primary record type (e.g. pages).
     #      This ensures the limit/offset isn't mispositioned by extra rows
     #      created when joining to the versions table.
@@ -16,21 +14,34 @@ class Api::V0::PagesController < Api::V0::ApiController
     #      This gets us the actual data to instantiate models.
     #
     # HOWEVER! If ordering criteria includes fields from the associated records
-    # (e.g. versions), then they those fields have to be included in the first
-    # query, which means we potentially get the wrong primary record IDs (since
-    # the returned rows are now a pages + versions combo).
+    # (e.g. versions), then those fields have to be included in the first query,
+    # which means we potentially get the wrong primary record IDs (since the
+    # returned rows are now a pages + versions combo).
     #
-    # So my best solution here is to sort in-app :(
+    # To work around this, do a similar operation manually:
+    #   1. Do NOT `includes(:versions)` and do NOT include version sorting on
+    #      the first query where we get IDs so as not to trigger the two-query
+    #      behavior (otherwise it would query for a set of IDs in order to query
+    #      for the IDs we asked for).
+    #   2. Once we have IDs, query specifically for those IDs and then include
+    #      the associated version records and version ordering info. This does
+    #      not trigger the two-query behavior because there's no limit/offset.
+    #
+    # NOTE: there was a previous solution to this that manually ordered
+    # versions right here in Ruby. This approach results in the same number of
+    # SQL queries and gets results already in the right order, so should
+    # hopefully be more performant.
     result_data =
       if should_include_versions
-        pages.as_json(include: :versions).collect do |page|
-          page['versions'].sort! do |a, b|
-            a['capture_time'] <=> b['capture_time']
-          end
-          page
-        end
+        # NOTE: need to get :updated_at here because it's used for ordering
+        page_ids = pages.pluck(:uuid, :updated_at).collect {|data| data[0]}
+        results = query
+          .where(uuid: page_ids)
+          .includes(:versions)
+          .order('versions.capture_time')
+        results.as_json(include: :versions)
       else
-        pages.as_json(include: :latest)
+        pages.includes(:latest).as_json(include: :latest)
       end
 
     render json: {
@@ -83,13 +94,6 @@ class Api::V0::PagesController < Api::V0::ApiController
         collection = collection.where(url: query)
       end
     end
-
-    collection =
-      if should_include_versions
-        collection.includes(:versions)
-      else
-        collection.includes(:latest)
-      end
 
     # If any queries create implicit joins, ensure we get a list of unique pages
     collection.distinct.order(updated_at: :desc)
