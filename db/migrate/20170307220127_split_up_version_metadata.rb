b602c107718d127d4581f0fd8bdc0dd141ac2ab9
Kate Donaldson <katelovescode@gmail.com>
2017-10-08 12:58:44 -0500

    Clean up migrations - db:setup seems to hang open after creating admin but the site looks like it's up - could use an additional tester

--- db/migrate/20170307220127_split_up_version_metadata.rb
+++
@@ -1,8 +0,0 @@
-class SplitUpVersionMetadata < ActiveRecord::Migration[5.0]
-  def change
-    change_table :versionista_versions do |t|
-      t.jsonb :annotations
-      t.rename :metadata, :current_annotation
-    end
-  end
-end
