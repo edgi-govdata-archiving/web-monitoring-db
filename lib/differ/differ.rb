70a6bacbb8b34f74dfa1c7120003ebb00edcc1a1
Rob Brackett <rob@robbrackett.com>
2017-12-09 15:34:21 -0800

    Add a "default" differ type using DIFFER_DEFAULT
    The value of the DIFFER_DEFAULT environment variable will be used as the URL for a new SimpleDiff instance if no diff has been registered for a requested diff type. You can still keep explicitly registered differs via any environment variables named `DIFFER_*`. They'll be matched before falling back to creating a default differ. This way, we can keep current configurations in place until the UI has updated to request the correct diff names (DB's names don't quite match processing names; see https://github.com/edgi-govdata-archiving/web-monitoring-processing/pull/133).
    
    Fixes #180. Fixes #182.

--- lib/differ/differ.rb
+++ lib/differ/differ.rb
@@ -1,5 +1,7 @@
 module Differ
-  # Register a differ instance to be used for a given diff type
+  # Register a differ instance to be used for a given diff type. If `type` is
+  # nil, the differ will be treated as the constructor arguments for a differ
+  # to use when no matching diff type is found for registry lookups.
   def self.register(type, differ)
     @type_map ||= {}.with_indifferent_access
     @type_map[type] = differ
@@ -7,11 +9,17 @@ module Differ
 
   # Retrieve the differ associated with a given diff type
   def self.for_type(type)
-    @type_map ? @type_map[type] : nil
+    @type_map && @type_map[type] || default_for_type(type)
   end
 
   def self.for_type!(type)
     for_type(type) || (raise Api::NotImplementedError,
       "There is no registered differ for '#{type}'")
+  end
+
+  # If configured, create a default/fallback differ for a given type.
+  def self.default_for_type(type)
+    default_url = @type_map && @type_map[nil]
+    default_url ? SimpleDiff.new(default_url, type) : nil
   end
 end
