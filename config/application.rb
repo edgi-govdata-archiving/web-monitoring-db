a8da8912f51b4a4332196c09b6b5781f599ff257
Rob Brackett <rob@robbrackett.com>
2018-01-13 17:00:25 -0800

    HOTFIX: move caching config to base app config
    Seems like the initializer was too late (?) so it takes better here. Still very confused about what wins when for cache configuration; I think Spring has really made it confusing locally.

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,14 @@ module WebpageVersionsDb
           :credentials => true
       end
     end
+
+    # Optional caching via Redis
+    if ENV['REDIS_CACHE_URL']
+      config.cache_store = :readthis_store, {
+        expires_in: 2.weeks.to_i,
+        namespace: 'wmdbcache',
+        redis: { url: ENV.fetch('REDIS_CACHE_URL'), driver: :hiredis }
+      }
+    end
   end
 end
--- config/initializers/cache.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.configure do
-  if ENV['REDIS_CACHE_URL']
-    config.cache_store = :readthis_store, {
-      expires_in: 2.weeks.to_i,
-      namespace: 'wmdbcache',
-      redis: { url: ENV.fetch('REDIS_CACHE_URL'), driver: :hiredis }
-    }
-  end
-end
