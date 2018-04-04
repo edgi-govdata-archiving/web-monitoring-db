b0a8e208917c6e70ae5e785d60d75f4445dd257d
Rob Brackett <rob@robbrackett.com>
2018-04-04 01:33:22 -0700

    Add retry/backoff to Archiver when downloading
    When downloading raw content, the archiver should retry (up to two additional times) for gateway errors and timeouts. If the final result was still a gateway error, we process it as a success anyhow -- its possible that the actual snapshotted response was a gateway error and we don't want to fail to save that.
    
    Fixes #261.

--- test/lib/archiver/archiver_test.rb
+++ test/lib/archiver/archiver_test.rb
@@ -8,6 +8,7 @@ class Archiver::ArchiverTest < ActiveSupport::TestCase
 
   def teardown
     Archiver.store = @original_storage
+    WebMock.reset!
   end
 
   test 'it saves the URL by its hash' do
@@ -38,5 +39,33 @@ class Archiver::ArchiverTest < ActiveSupport::TestCase
     assert_raises(Api::ApiError) do
       Archiver.archive('http://example.com', expected_hash: 'abc')
     end
+  end
+
+  test 'it should retry on HTTP errors and gateway errors' do
+    request = stub_request(:get, 'http://example.com')
+      .to_return(body: 'Gateway Error', status: 503).then
+      .to_return(body: 'Hello!', status: 200)
+
+    result = Archiver.archive('http://example.com')
+    assert_requested(request, times: 2)
+    assert_equal('Hello!', Archiver.store.get_file(result[:hash]))
+  end
+
+  test 'it should save error content after the maximum number of retries' do
+    request = stub_request(:get, 'http://example.com')
+      .to_return(body: 'Gateway Error', status: 503).times(3)
+
+    result = Archiver.archive('http://example.com')
+    assert_requested(request, times: 3)
+    assert_equal('Gateway Error', Archiver.store.get_file(result[:hash]))
+  end
+
+  test 'it hashes the content at a URL' do
+    hash = '334d016f755cd6dc58c53a86e183882f8ec14f52fb05345887c8a5edd42c87b7'
+    stub_request(:any, 'http://example.com')
+      .to_return(body: 'Hello!', status: 200)
+
+    result = Archiver.hash_content_at_url('http://example.com')
+    assert_equal(hash, result)
   end
 end
