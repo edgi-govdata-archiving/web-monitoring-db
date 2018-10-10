1c380c7b773ca2068af3a8a79394fdb557499639
Dan Allan <daniel.b.allan@gmail.com>
2018-10-10 09:50:15 -0400

    Check that database and queue are available in healthcheck.

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,5 +1,9 @@
 class HealthcheckController < ApplicationController
   def index
+    # Ensure database is available
+    ActiveRecord::Base.connection.exec_query('SELECT 2 + 2;')
+    # Ensure job queue is available
+    Resque.size(:not_a_real_queue_but_thats_ok)
     render json: {}
   end
 end
