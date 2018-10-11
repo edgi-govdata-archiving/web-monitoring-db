fa72560b3b10192161e9030eb3f7a3987479c6e3
Dan Allan <daniel.b.allan@gmail.com>
2018-10-11 10:40:18 -0400

    Succeed if the app is up. Provide additional info in response.

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -1,9 +1,21 @@
 class HealthcheckController < ApplicationController
   def index
-    # Ensure database is available
-    ActiveRecord::Base.connection.exec_query('SELECT 2 + 2;')
-    # Ensure job queue is available
-    Resque.size(:not_a_real_queue_but_thats_ok)
-    render json: {}
+    # Succeed if the app is running, but provide additional
+    # info if related components have gone down.
+    db = 'ok'
+    begin
+      ActiveRecord::Base.connection.exec_query('SELECT 2 + 2;')
+    rescue StandardError => error
+      db = error.to_s
+    end
+
+    queues = 'ok'
+    begin
+      Resque.size(:not_a_real_queue_but_thats_ok)
+    rescue StandardError => error
+      queues = error.to_s
+    end
+
+    render json: {app: 'ok', db: db, queues: queues}
   end
 end
