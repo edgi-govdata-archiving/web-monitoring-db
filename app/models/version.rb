c455ae97569b895d8b7d846e92db38f70936a396
Rob Brackett <rob@robbrackett.com>
2018-11-25 20:23:59 -0800

    Ensure version/page titles are nicely formatted
    Version and Page titles should conform to some simple rules around formatting: namely that any surrounding spaces should be removed and that they are a single line. This also goes a step farther and normalizes repeated spaces (but not non-breaking spaces) into a single space, similar to HTML. This is mainly a backstop for import processes that might send us poorly formatted titles (perhaps by doing a poor job parsing HTML :(
    
    I think I might also like to extend this concern to cover Tags and Maintainers in the future -- it seems reasonable that we'd want to apply the same rules to the `name` property on those models.
    
    Fixes #430.

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,5 +1,6 @@
 class Version < ApplicationRecord
   include UuidPrimaryKey
+  include SimpleTitle
 
   belongs_to :page, foreign_key: :page_uuid, required: true, inverse_of: :versions, touch: true
   has_many :tracked_changes, class_name: 'Change', foreign_key: 'uuid_to'
