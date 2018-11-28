0733cd99287dc39d94d4c14c99380f7fb27f17d0
Jason Sherman <jsn.sherman@gmail.com>
2018-11-28 07:51:31 -0600

    don't hardcode storage path in raw versions controller.

--- app/controllers/api/v0/raw_controller.rb
+++ app/controllers/api/v0/raw_controller.rb
@@ -2,9 +2,8 @@ class Api::V0::RawController < Api::V0::ApiController
 
   def show
     @version ||= Version.find_by(version_hash: params[:id])
-    # TODO: only return a local file if a bucket isn't set. Otherwise redirect to bucket
-    # TODO: don't hardcode path
-    path = '/app/tmp/storage/archive/' + @version.version_hash
+    storage_path = Rails.root.join 'tmp/storage/archive'
+    path = "#{storage_path}/#{@version.version_hash}"
     mime_type = @version.source_metadata['mime_type']
     send_file(path, type: mime_type, disposition: 'inline')
   end
