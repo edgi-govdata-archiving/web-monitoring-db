af015f5f4e78b279b288d82291158510813cf9a0
Rob Brackett <rob@robbrackett.com>
2019-02-21 19:04:16 -0800

    Make mailer test work with different hosts
    My local test environment has an unusual HOST environment var, and the previous test didn't pick it up. This uses the helpers to make sure the URL is constructed correctly, but has to employ a few hacks to to make sure the mailer-specific config is getting picked up :\

--- test/mailers/invitation_mailer_test.rb
+++ test/mailers/invitation_mailer_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class InvitationMailerTest < ActionMailer::TestCase
+  # Make it possible to generate URLs. In mailer tests, URL helpers are missing
+  # the action_mailer URL configuration.
+  # NOTE: this is handled automatically in RSpec
+  include Rails.application.routes.url_helpers
+  def mail_url(route, options = nil)
+    url_for([
+      route,
+      Rails.application.config.action_mailer.default_url_options.merge(options)
+    ])
+  end
+
   def test_send_invitation_email
     invitation = Invitation.create(email: "test@email.com")
     invitation.update_attributes(code: "eff7df8ccc0cd0256619")
@@ -13,7 +24,9 @@ class InvitationMailerTest < ActionMailer::TestCase
     assert_equal "Welcome to EDGI web monitoring", email.subject
     assert_equal [ENV.fetch('MAIL_SENDER', 'web-monitoring-db@envirodatagov.org')], email.from
     assert_equal ["test@email.com"], email.to
-    assert_match /http:\/\/web-monitoring-db.test\/users\/sign_up\?invitation=\w+/, email.html_part.body.to_s
-    assert_match /http:\/\/web-monitoring-db.test\/users\/sign_up\?invitation=\w+/, email.text_part.body.to_s
+
+    invite_url = mail_url(:new_user_registration, invitation: invitation.code)
+    assert_includes email.html_part.body.to_s, invite_url
+    assert_includes email.text_part.body.to_s, invite_url
   end
 end
