cfa1d358bb3129e33d35d5e361646fe388969e1a
Rob Brackett <rob@robbrackett.com>
2019-05-31 11:43:14 -0700

    Add migration to drop the `admin` column for users (#536)
    Fixes #34 (Most of the work was in #533, this just cleans up the last bit!)

---
+++ db/migrate/20190531175430_remove_admin_from_users.rb
@@ -0,0 +1,10 @@
+class RemoveAdminFromUsers < ActiveRecord::Migration[5.2]
+  def up
+    remove_column :users, :admin
+  end
+
+  def down
+    add_column :users, :admin, :boolean, default: false
+    execute "UPDATE users SET admin = TRUE WHERE 'manage_users' = ANY(permissions);"
+  end
+end
