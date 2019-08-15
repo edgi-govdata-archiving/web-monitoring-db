8cf03de018df562c28a782f43d2be5c2e53b2f09
Rob Brackett <rob@robbrackett.com>
2019-08-15 14:22:30 -0700

    Add indexes for commonly searched fields on Version (#548)
    We really commonly filter by several fields on the versions table that don't have indexes, and that causes some real slowdowns. This adds indexes for them.

---
+++ db/migrate/20190617231901_add_common_indexes_to_versions.rb
@@ -0,0 +1,20 @@
+class AddCommonIndexesToVersions < ActiveRecord::Migration[5.2]
+  # Add indexes to support some common queries we do that should be faster.
+  def change
+    # Our default sorting is by created_at:asc.
+    add_index :versions, :created_at
+    # We also search by source_type pretty frequently.
+    add_index :versions, :source_type
+
+    # In most cases, we only search for records where different = true.
+    # However, an index for `different` or a compound index featuring it
+    # will almost never get used in practice! Instead, create partial
+    # indexes, which do get used.
+    add_index :versions, :capture_time,
+              where: 'different = true',
+              name: 'index_different_versions_on_capture_time'
+    add_index :versions, :created_at,
+              where: 'different = true',
+              name: 'index_different_versions_on_created_at'
+  end
+end
