04ed3138005c920b7520fdc571aae1d8c3fd5124
Jason Sherman <jsnshrmn@users.noreply.github.com>
2019-10-21 11:15:31 -0500

    upstream.content_type no longer a thing
    in raw version controller.

--- app/controllers/api/v0/versions_controller.rb
+++ app/controllers/api/v0/versions_controller.rb
@@ -37,7 +37,7 @@ class Api::V0::VersionsController < Api::V0::ApiController
       filename = File.basename(@version.uri)
       upstream = Archiver.store.get_file(filename)
       # Try to get the filetype, fall back on binary.
-      type = version_media_type(@version) || upstream.content_type || 'application/octet-stream'
+      type = version_media_type(@version) || 'application/octet-stream'
       # Set binary file disposition to attachment; anything else is inine.
       disposition = type == 'application/octet-stream' ? 'attachment' : 'inline'
 
