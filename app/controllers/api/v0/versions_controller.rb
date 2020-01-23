ef13cea90f4fcb8174f6ed78aeb0f2bb46700845
Rob Brackett <rob@robbrackett.com>
2020-01-22 21:25:10 -0800

    Allow FileStorage get_file methods to take URLs
    The `get_file` method on `FileStorage:S3` and `FileStorage:LocalFile` can now take a local path, or a URL/URI. If the input specifies a location outside the storage area, they raise an exception.

--- app/controllers/api/v0/versions_controller.rb
+++ app/controllers/api/v0/versions_controller.rb
@@ -36,9 +36,9 @@ class Api::V0::VersionsController < Api::V0::ApiController
       redirect_to @version.uri, status: 301
     elsif Archiver.store.contains_url?(@version.uri)
       # Get the file
-      path = URI.parse(@version.uri).path
-      filename = File.basename(path)
-      upstream = Archiver.store.get_file(filename)
+      filename = File.basename(@version.uri)
+      upstream = Archiver.store.get_file(@version.uri)
+
       # Try to get the filetype, fall back on binary.
       type = version_media_type(@version) || 'application/octet-stream'
       # Set binary file disposition to attachment; anything else is inline.
