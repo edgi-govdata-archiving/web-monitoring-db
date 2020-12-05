f455f5b93eeff46d6b187852eb72904ffc668439
Rob Brackett <rob@robbrackett.com>
2020-12-04 21:21:11 -0800

    Add hack to fix ActiveRecord schema.rb bug
    This is required for date ranges involving +/-infinity, which we are about to add for tracking multiple URLs per page.

---
+++ config/initializers/active_record_extras.rb
@@ -0,0 +1,22 @@
+# DateTime types do not serialize the values for +/-infinity
+# correctly for `schema.rb`. This hack gives us a functional
+# schema.rb file.
+# See also:
+#   https://github.com/rails/rails/issues/40751
+module ActiveRecord
+  module ConnectionAdapters
+    module PostgreSQL
+      module OID
+        class DateTime < Type::DateTime
+          def type_cast_for_schema(value)
+            case value
+              when ::Float::INFINITY then '::Float::INFINITY'
+              when -::Float::INFINITY then '-::Float::INFINITY'
+              else super
+            end
+          end
+        end
+      end
+    end
+  end
+end
