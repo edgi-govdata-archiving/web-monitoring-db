68f0ab59798d6e5400bd47a4c3116524bf2de0cc
Rob Brackett <rob@robbrackett.com>
2021-06-05 17:41:25 -0700

    Don't rename `content_length`

--- db/migrate/20210605234433_rename_version_url_uri_hash_length_headers.rb
+++ db/migrate/20210605234433_rename_version_url_uri_hash_length_headers.rb
@@ -4,9 +4,6 @@ class RenameVersionUrlUriHashLengthHeaders < ActiveRecord::Migration[6.1]
       t.rename :capture_url, :url
       t.rename :uri, :body_url
       t.rename :version_hash, :body_hash
-      # XXX: decide whether to do this one
-      # t.rename :content_length, :body_length
-
       # This will need a corresponding data migration to backfill
       # from source_metadata.headers when present.
       t.jsonb :headers
