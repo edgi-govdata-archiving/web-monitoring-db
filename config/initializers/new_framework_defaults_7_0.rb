ba99c6ab57da2177272990d60e658317f3a230af
Rob Brackett <rob@robbrackett.com>
2022-09-01 18:39:17 -0700

    We only use UUID4, so let other stuff be the default

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -106,7 +106,7 @@ Rails.application.config.active_storage.variant_processor = :vips
 #
 # See https://guides.rubyonrails.org/configuring.html#config-active-support-use-rfc4122-namespaced-uuids for
 # more information.
-# Rails.application.config.active_support.use_rfc4122_namespaced_uuids = true
+Rails.application.config.active_support.use_rfc4122_namespaced_uuids = true
 
 # Change the default headers to disable browsers' flawed legacy XSS protection.
 # Rails.application.config.action_dispatch.default_headers = {
