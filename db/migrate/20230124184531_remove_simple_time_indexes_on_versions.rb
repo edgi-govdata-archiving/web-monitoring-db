dd684ececdde5878a23aaef9042713225c90952b
Rob Brackett <rob@robbrackett.com>
2023-01-25 11:30:26 -0800

    Oops, committed botched version of migration

--- db/migrate/20230124184531_remove_simple_time_indexes_on_versions.rb
+++ db/migrate/20230124184531_remove_simple_time_indexes_on_versions.rb
@@ -3,15 +3,11 @@ class RemoveSimpleTimeIndexesOnVersions < ActiveRecord::Migration[7.0]
     # Drop indexes on Version time fields. These are now redundant since we
     # have compound indexes (with UUID) that can be used in the cases these
     # were designed for.
-    remove_index(:versions, :created_at)
-    remove_index(:versions, :capture_time)
+    remove_index(:versions, name: 'index_versions_on_created_at')
+    remove_index(:versions, name: 'index_versions_on_capture_time')
 
     # Similarly, these partial indexes are no longer heavily used
-    remove_index(:versions, :capture_time,
-                 where: 'different = true',
-                 name: 'index_different_versions_on_capture_time')
-    remove_index(:versions, :created_at,
-                 where: 'different = true',
-                 name: 'index_different_versions_on_created_at')
+    remove_index(:versions, name: 'index_different_versions_on_created_at')
+    remove_index(:versions, name: 'index_different_versions_on_capture_time')
   end
 end
