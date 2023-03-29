1c0b9d262005f4ba007ed9d8e29d512e62b7d885
Rob Brackett <rob@robbrackett.com>
2023-03-28 17:04:48 -0700

    Remove Resque

--- config/initializers/resque.rb
+++
@@ -1,7 +0,0 @@
-# Auto-configuration of Redis connections based on REDIS_URL seems to have
-# broken in either v4 or the Redis gem or v1.6 of redis-namespace. Manually fix
-# up the configuration instead here.
-if ENV['REDIS_URL']
-  Rails.logger.debug('Configuring Redis URL')
-  Resque.redis = Redis.new(url: ENV['REDIS_URL'])
-end
