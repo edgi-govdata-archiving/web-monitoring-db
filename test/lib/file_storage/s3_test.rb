5689d622da49530a04cce766d1f79ad2b5c337c2
Rob Brackett <rob@robbrackett.com>
2024-11-26 17:15:53 -0800

    Fix test warnings

--- test/lib/file_storage/s3_test.rb
+++ test/lib/file_storage/s3_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class FileStorage::S3Test < ActiveSupport::TestCase
-  def test_storage
+  def example_storage
     FileStorage::S3.new(
       key: 'whatever',
       secret: 'test',
@@ -11,31 +11,31 @@ class FileStorage::S3Test < ActiveSupport::TestCase
   end
 
   test 's3 storage can parse S3 URIs' do
-    parsed = test_storage.send :parse_s3_url, 's3://test-bucket/some/thing.txt'
+    parsed = example_storage.send :parse_s3_url, 's3://test-bucket/some/thing.txt'
     assert_equal 'test-bucket', parsed[:bucket]
     assert_equal 'some/thing.txt', parsed[:path]
     assert_nil parsed[:region]
   end
 
   test 's3 storage can parse S3 URLs' do
-    bucket_hostname_url = test_storage.send :parse_s3_url, 'https://test-bucket.s3.amazonaws.com/some/thing.txt'
+    bucket_hostname_url = example_storage.send :parse_s3_url, 'https://test-bucket.s3.amazonaws.com/some/thing.txt'
     assert_equal 'test-bucket', bucket_hostname_url[:bucket]
     assert_equal 'some/thing.txt', bucket_hostname_url[:path]
     assert_nil bucket_hostname_url[:region]
 
-    bucket_path_url = test_storage.send :parse_s3_url, 'https://s3.amazonaws.com/test-bucket/some/thing.txt'
+    bucket_path_url = example_storage.send :parse_s3_url, 'https://s3.amazonaws.com/test-bucket/some/thing.txt'
     assert_equal 'test-bucket', bucket_path_url[:bucket]
     assert_equal 'some/thing.txt', bucket_path_url[:path]
     assert_nil bucket_path_url[:region]
 
-    region_url = test_storage.send :parse_s3_url, 'https://s3-us-west-2.amazonaws.com/test-bucket/some/thing.txt'
+    region_url = example_storage.send :parse_s3_url, 'https://s3-us-west-2.amazonaws.com/test-bucket/some/thing.txt'
     assert_equal 'test-bucket', region_url[:bucket]
     assert_equal 'some/thing.txt', region_url[:path]
     assert_equal 'us-west-2', region_url[:region]
   end
 
   test 's3 storage will not parse non-S3 URLs' do
-    parsed = test_storage.send :parse_s3_url, 'https://google.com/some/thing.txt'
+    parsed = example_storage.send :parse_s3_url, 'https://google.com/some/thing.txt'
     assert_nil parsed
   end
 
@@ -45,7 +45,7 @@ class FileStorage::S3Test < ActiveSupport::TestCase
     stub_request(:head, 'https://test-bucket.s3.us-west-2.amazonaws.com/does-not-exist.txt')
       .to_return(status: 404, body: '', headers: {})
 
-    storage = test_storage
+    storage = example_storage
     assert storage.contains_url?('s3://test-bucket/something.txt')
     assert_not storage.contains_url?('s3://test-bucket/does-not-exist.txt')
     # No stub because no request should be made (it's in the wrong bucket)
@@ -58,7 +58,7 @@ class FileStorage::S3Test < ActiveSupport::TestCase
     stub_request(:head, 'https://test-bucket.s3.us-west-2.amazonaws.com/does-not-exist.txt')
       .to_return(status: 404, body: '', headers: {})
 
-    storage = test_storage
+    storage = example_storage
     assert storage.contains_url?('https://test-bucket.s3.amazonaws.com/something.txt')
     assert_not storage.contains_url?('s3://test-bucket/does-not-exist.txt')
     # No stub because no request should be made (it's in the wrong bucket)
@@ -66,7 +66,7 @@ class FileStorage::S3Test < ActiveSupport::TestCase
   end
 
   test 'can generate a URL' do
-    whatever_url = test_storage.url_for_file('whatever')
+    whatever_url = example_storage.url_for_file('whatever')
     assert_equal 'https://test-bucket.s3.amazonaws.com/whatever', whatever_url
   end
 end
