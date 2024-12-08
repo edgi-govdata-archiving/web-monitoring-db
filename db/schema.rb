8c3c2fd43cf28ed44d995fd2b90750c8286ea429
Rob Brackett <rob@robbrackett.com>
2024-12-08 13:34:36 -0800

    Upgrade Rails to v8 (#1164)
    The final update to get Rails up-to-date. This should mostly get things ready for continued development if/as new web monitoring work starts for another Trump term.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_20_215224) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_20_215224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
   create_table "annotations", primary_key: "uuid", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
