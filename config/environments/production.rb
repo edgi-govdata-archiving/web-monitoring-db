e5b6693102adc2fcec6c2b7fabe0d17c0c3ec4e7
Rob Brackett <rob@robbrackett.com>
2025-01-10 13:15:25 -0800

    Fix e-mail invitation expiration and confirmation issues (#1176)
    Fixes a few minor issues with e-mail confirmation I noticed while trying to solve some Postmark issues. None of these are causing actual problems in practice, though.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_key: ENV.fetch('POSTMARK_API_TOKEN')
+    api_token: ENV.fetch('POSTMARK_API_TOKEN')
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
