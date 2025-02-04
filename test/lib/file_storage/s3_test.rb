edgi-govdata-archiving/web-monitoring-db

commit 6bfe7942bce2d07e942bd8c87205e06244fed201

Handle gzipped data in S3 (#1185)
This adds the ability to handle data in S3 that is gzipped. It also sets things up to automatically gzip anything we put in S3 to save space.

lib/file_storage/s3.rb
@@ -2,7 +2,7 @@ require 'aws-sdk-s3'
 
 module FileStorage
   class S3
-    def initialize(key: nil, secret: nil, bucket:, region: nil, acl: 'public-read')
+    def initialize(key: nil, secret: nil, bucket:, region: nil, acl: 'public-read', gzip: false)
       @bucket = bucket
       @region = region || 'us-east-1'
       @client = Aws::S3::Client.new(
@@ -11,6 +11,7 @@ module FileStorage
         region: @region
       )
       @acl = acl
+      @gzip = gzip
     end
 
     def contains_url?(url_string)
@@ -31,13 +32,20 @@ module FileStorage
       {
         last_modified: data.last_modified,
         size: data.content_length,
-        content_type: data.content_type
+        content_type: data.content_type,
+        content_encoding: data.content_encoding
       }
     end
 
     def get_file(path)
       bucket_path = normalize_full_path(path)
-      @client.get_object(bucket: @bucket, key: bucket_path).body.read
+      object = @client.get_object(bucket: @bucket, key: bucket_path)
+
+      if object.content_encoding == 'gzip'
+        Zlib::GzipReader.zcat(object.body)
+      else
+        object.body.read
+      end
     end
 
     def save_file(path, content, options = nil)
@@ -45,9 +53,10 @@ module FileStorage
       response = @client.put_object(
         bucket: @bucket,
         key: path,
-        body: content,
+        body: @gzip ? ActiveSupport::Gzip.compress(content) : content,
         acl: options.fetch(:acl, @acl),
-        content_type: options.fetch(:content_type, 'application/octet-stream')
+        content_type: options.fetch(:content_type, 'application/octet-stream'),
+        content_encoding: @gzip ? 'gzip' : nil
       )
       { url: url_for_file(path), meta: response }
     end

test/lib/file_storage/s3_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class FileStorage::S3Test < ActiveSupport::TestCase
-  def example_storage
+  def example_storage(gzip: false)
     FileStorage::S3.new(
       key: 'whatever',
       secret: 'test',
       bucket: 'test-bucket',
-      region: 'us-west-2'
+      region: 'us-west-2',
+      gzip: gzip
     )
   end
 
@@ -68,5 +69,49 @@ class FileStorage::S3Test < ActiveSupport::TestCase
   test 'can generate a URL' do
     whatever_url = example_storage.url_for_file('whatever')
     assert_equal 'https://test-bucket.s3.amazonaws.com/whatever', whatever_url
+  end
+
+  test 's3 storage can get a file' do
+    stub_request(:get, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+      .to_return(status: 200, body: 'Hello from S3!', headers: {})
+
+    storage = example_storage
+    assert_equal 'Hello from S3!', storage.get_file('https://test-bucket.s3.amazonaws.com/something.txt')
+  end
+
+  test 's3 storage can get a gzipped file' do
+    stub_request(:get, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+      .to_return(status: 200, body: ActiveSupport::Gzip.compress('Hello from S3!'), headers: { 'Content-Encoding' => 'gzip' })
+
+    storage = example_storage
+    assert_equal 'Hello from S3!', storage.get_file('https://test-bucket.s3.amazonaws.com/something.txt')
+  end
+
+  test 's3 storage can write a file' do
+    text = 'Hello from S3!'
+
+    s3_gzipped_put = stub_request(:put, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+      .with(body: text, headers: { 'Content-Type' => 'text/plain', 'Content-Encoding' => 'gzip' })
+      .to_return(status: 200, body: '', headers: {})
+    s3_put = stub_request(:put, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+      .with(body: text, headers: { 'Content-Type' => 'text/plain' })
+      .to_return(status: 200, body: '', headers: {})
+
+    storage = example_storage
+    storage.save_file('something.txt', text, content_type: 'text/plain')
+    assert_requested(s3_put)
+    assert_not_requested(s3_gzipped_put)
+  end
+
+  test 's3 storage can write a gzipped file' do
+    text = 'Hello from S3!'
+
+    s3_put = stub_request(:put, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+      .with(body: ActiveSupport::Gzip.compress(text), headers: { 'Content-Type' => 'text/plain', 'Content-Encoding' => 'gzip' })
+      .to_return(status: 200, body: '', headers: {})
+
+    storage = example_storage(gzip: true)
+    storage.save_file('something.txt', text, content_type: 'text/plain')
+    assert_requested(s3_put)
   end
 end

config/initializers/archiver.rb
@@ -16,7 +16,8 @@ if ENV['AWS_ARCHIVE_BUCKET']
     key: aws_key,
     secret: aws_secret,
     region: aws_region,
-    bucket: aws_bucket
+    bucket: aws_bucket,
+    gzip: true
   )
 elsif !Rails.env.production?
   directory = Rails.env.test? ? 'archive-test' : 'archive'

config/initializers/file_storage.rb
@@ -5,7 +5,8 @@ if ENV['AWS_WORKING_BUCKET']
   FileStorage.default = FileStorage::S3.new(
     bucket: ENV['AWS_WORKING_BUCKET'],
     region: ENV['AWS_WORKING_REGION'] || ENV['AWS_REGION'],
-    acl: 'private'
+    acl: 'private',
+    gzip: true
   )
 else
   storage_path = Rails.root.join('tmp/storage/working')

commit 06b10b3faba7132826e27c82ade5fb0feb98129f

Fix gzip writing bug introduced in #1185
In #1185, we added support for reading and writing gzipped data to S3. Unfortunately, the support only worked when writing a *string*, but not when writing an IO stream. It turns out that when saving a batched import to execute in a job, we write a stream. Oops.

lib/file_storage/s3.rb
@@ -53,7 +53,8 @@ module FileStorage
       response = @client.put_object(
         bucket: @bucket,
         key: path,
-        body: @gzip ? ActiveSupport::Gzip.compress(content) : content,
+        # TODO: it would be nice to support streaming through the gzip compressor instead of buffering all `content`.
+        body: @gzip ? ActiveSupport::Gzip.compress(content.try(:read) || content) : content,
         acl: options.fetch(:acl, @acl),
         content_type: options.fetch(:content_type, 'application/octet-stream'),
         content_encoding: @gzip ? 'gzip' : nil

test/lib/file_storage/s3_test.rb
@@ -114,4 +114,16 @@ class FileStorage::S3Test < ActiveSupport::TestCase
     storage.save_file('something.txt', text, content_type: 'text/plain')
     assert_requested(s3_put)
   end
+
+  test 's3 storage can write a gzipped stream' do
+    text = 'Hello from S3!'
+
+    s3_put = stub_request(:put, 'https://test-bucket.s3.us-west-2.amazonaws.com/something.txt')
+               .with(body: ActiveSupport::Gzip.compress(text), headers: { 'Content-Type' => 'text/plain', 'Content-Encoding' => 'gzip' })
+               .to_return(status: 200, body: '', headers: {})
+
+    storage = example_storage(gzip: true)
+    storage.save_file('something.txt', StringIO.new(text), content_type: 'text/plain')
+    assert_requested(s3_put)
+  end
 end