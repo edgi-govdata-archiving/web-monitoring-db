6d86bdd38fe192e0ccda6750f56e609dbc83d5c2
Rob Brackett <rob@robbrackett.com>
2025-08-01 21:06:20 -0700

    OBEY RUBOCOP

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   include UuidPrimaryKey
+
   has_many :taggings, foreign_key: :tag_uuid
   has_many :pages, through: :taggings, source: :taggable, source_type: :Page
 end
