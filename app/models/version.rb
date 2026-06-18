18c47f041c2506f1b30274957437b3bd58b410f2
Rob Brackett <rob@robbrackett.com>
2026-06-18 00:27:27 -0700

    Don't raise in `redirects`, it's too dangerous

--- app/models/version.rb
+++ app/models/version.rb
@@ -294,7 +294,12 @@ class Version < ApplicationRecord
 
   def redirects
     urls = source_metadata['redirects'] || []
-    raise TypeError, "Unknown type for source_metadata.redirects on version: #{uuid}" unless urls.is_a?(Array)
+    unless urls.is_a?(Array)
+      message = "Invalid `source_metadata.redirects` for version #{uuid}"
+      Rails.logger.error(message)
+      Sentry.capture_message(message, level: :error)
+      urls = []
+    end
 
     # TODO: add option to fetch raw body and look for client redirects? FWIW, data from the EDGI crawler already
     #  includes these.
